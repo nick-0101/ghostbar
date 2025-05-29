9e2511a04c77f30156e728bd9026c62bbeeef29f
nick-0101 <54145857+nick-0101@users.noreply.github.com>
2025-05-28 23:28:32 -0400

    ++

--- src/components/ui/input.vue
+++ src/components/ui/input.vue
@@ -25,7 +25,7 @@ const modelValue = useVModel(props, "modelValue", emits, {
 
 <template>
   <div className="relative flex items-center">
-    <input v-bind="$attrs" v-model="modelValue" :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-foreground file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', props.class)" />
+    <input v-bind="$attrs" v-model="modelValue" :class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-foreground file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 pr-10', props.class)" />
     <div className="absolute right-3">
       <slot name="icon" />
     </div>
