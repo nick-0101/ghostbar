433af157ec9cc59b38167d0183e68272f50386cc
nick-0101 <nicolapirruccio@icloud.com>
2025-06-07 15:00:05 -0400

    got HMR reloading

---
+++ src/content/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col py-4 border-b w-[350px] h-[400px] bg-gray-50/40 font-inter">Some test content sdfsdfsdhere</div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
-import { resolve } from "path";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+// import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,20 +13,25 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  plugins: [vue(), tailwindcss(), crx({ manifest }), cssInjectedByJsPlugin()],
-  build: {
-    cssCodeSplit: false,
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, "index.html"),
-        content: resolve(__dirname, "src/content/content.js"),
-        background: resolve(__dirname, "src/background.js"),
-      },
-      output: {
-        entryFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
-        chunkFileNames: "[name].js",
-      },
-    },
-  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+    crx({ manifest }),
+    // cssInjectedByJsPlugin()
+  ],
+  // build: {
+  //   cssCodeSplit: false,
+  //   rollupOptions: {
+  //     input: {
+  //       popup: resolve(__dirname, "index.html"),
+  //       content: resolve(__dirname, "src/content/content.js"),
+  //       background: resolve(__dirname, "src/background.js"),
+  //     },
+  //     output: {
+  //       entryFileNames: "[name].js",
+  //       assetFileNames: "[name].[ext]",
+  //       chunkFileNames: "[name].js",
+  //     },
+  //   },
+  // },
 });
