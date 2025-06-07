516d01290df5179a034a3a69b8c4a9779863abd7
nick-0101 <nicolapirruccio@icloud.com>
2025-06-07 16:51:23 -0400

    moved component into vue file

---
+++ src/content/content.ts
@@ -0,0 +1,10 @@
+import "./style.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const root = document.createElement("div");
+root.id = "crx-root";
+document.body.append(root);
+
+const app = createApp(App);
+app.mount(root);
