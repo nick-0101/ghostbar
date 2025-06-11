984d6bec47ac14a4be3d8b5b717246fd59318a87
nick-0101 <nicolapirruccio@icloud.com>
2025-06-10 20:34:41 -0400

    building content selector

---
+++ src/content/components/ContentSelector.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
+
+const cursorPosition = ref({ x: 0, y: 0 });
+
+function handleMouseMove(event: MouseEvent) {
+  cursorPosition.value = { x: event.clientX, y: event.clientY };
+}
+
+onMounted(() => {
+  document.addEventListener("mousemove", handleMouseMove);
+});
+
+onUnmounted(() => {
+  document.removeEventListener("mousemove", handleMouseMove);
+});
+</script>
+
+<template>
+  <div class="ghostbar-selector" :style="{ top: `${cursorPosition.y}px`, left: `${cursorPosition.x}px` }">
+    <div class="ghostbar-content-selector-content">
+      <h1>Content Selector</h1>
+      <p>Click to select content</p>
+    </div>
+  </div>
+</template>
