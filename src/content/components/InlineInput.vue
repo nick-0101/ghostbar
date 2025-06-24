a1abad6853288e4acfd757553734c26b86b07ca4
nick-0101 <nicolapirruccio@icloud.com>
2025-06-23 23:06:45 -0400

    added textarea styles

--- src/content/components/InlineInput.vue
+++ src/content/components/InlineInput.vue
@@ -58,7 +58,7 @@ onUnmounted(() => {
 <template>
   <div class="ghostbar-inline-input-container" @mousedown="handleMouseDown">
     <div class="ghostbar-inline-input-container-inner Card">
-      <textarea id="ghostbar-inline-input-textarea" class="Input" type="text" placeholder="Ask anything" v-model="currentQuery" />
+      <textarea id="ghostbar-inline-input-textarea" class="Textarea" type="text" placeholder="Ask anything" v-model="currentQuery" />
       <button class="Button ghostbar-inline-input-search-button" variant="icon" @click.stop="handleExecuteQuery">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="m5 12 7-7 7 7" />
