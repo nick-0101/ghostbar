eddc2c2c06344caef7fddc2168facd432d458d51
nick-0101 <nicolapirruccio@icloud.com>
2025-06-28 17:14:55 -0400

    ++

--- src/content/components/InlineInput.vue
+++ src/content/components/InlineInput.vue
@@ -4,18 +4,15 @@ import { useUserConversationsStore } from "@/stores/conversations";
 import { storeToRefs } from "pinia";
 import { usePortStore } from "@/stores/portStore";
 import type { IExecuteQueryMessage } from "@/types";
+import Popover from "./ui/popover.vue";
 
 const cursorPosition = ref({ x: window.innerWidth / 2, y: window.innerHeight - 100 });
 const isDragging = ref(false);
 const dragOffset = ref({ x: 0, y: 0 });
 const userConversationsStore = useUserConversationsStore();
-const { conversations } = storeToRefs(userConversationsStore);
 const { sendMessage } = usePortStore();
 const searchQuery = ref("");
-
-const props = defineProps<{
-  selectedText: string;
-}>();
+const isPopoverOpen = ref(false);
 
 const handleExecuteQuery = () => {
   userConversationsStore.addUserQueryToConversation(searchQuery.value);
@@ -48,6 +45,28 @@ const handleMouseUp = () => {
   isDragging.value = false;
 };
 
+const handlePopoverChange = (value: boolean) => {
+  console.log(value, "from handlePopoverChange");
+  isPopoverOpen.value = value;
+};
+
+const availableAiModels = computed(() => {
+  return [
+    {
+      name: "GPT-4o",
+      subtitle: "Great for most tasks",
+    },
+    {
+      name: "gpt-4o-mini",
+      subtitle: "Smaller model, cheaper",
+    },
+    {
+      name: "gpt-4.1",
+      subtitle: "Newest model, best for complex tasks",
+    },
+  ];
+});
+
 onMounted(() => {
   window.addEventListener("mousemove", handleMouseMove);
   window.addEventListener("mouseup", handleMouseUp);
@@ -64,7 +83,19 @@ onUnmounted(() => {
     <div class="ghostbar-inline-input-container-inner Card">
       <textarea id="ghostbar-inline-input-textarea" class="Textarea" type="text" placeholder="Ask anything" v-model="searchQuery" />
       <div class="ghostbar-inline-input-actions-container">
-        <button class="ghostbar-inline-input-model-selector Button" data-variant="ghost">Model: 4o</button>
+        <Popover trigger="click" placement="top" :open="isPopoverOpen" @update:open="handlePopoverChange">
+          <template #trigger>
+            <button class="ghostbar-inline-input-model-selector Button" data-variant="ghost">Model: {{ userConversationsStore.selectedAiModel }}</button>
+          </template>
+
+          <div class="model-options">
+            <button v-for="model in availableAiModels" :key="model" class="model-option" :class="{ active: userConversationsStore.selectedAiModel === model }" @click="userConversationsStore.selectedAiModel = model">
+              {{ model }}
+              <p>{}</p>
+            </button>
+          </div>
+        </Popover>
+
         <button class="ghostbar-inline-input-search-button" @click.stop="handleExecuteQuery">
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <path d="m5 12 7-7 7 7" />
