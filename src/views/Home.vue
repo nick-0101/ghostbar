3755967c0297776e29f2bc7abf122c0dc035ed58
nick-0101 <nicolapirruccio@icloud.com>
2025-07-03 11:06:20 -0400

    added mint docs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import Container from '@/components/container.vue'
+import { ExternalLink } from 'lucide-vue-next'
 </script>
 
 <template>
   <Container class="pt-4">
     <template #default>
-      <h2 class="text-sm leading-none font-medium text-foreground mb-2">Intructions</h2>
+      <h2 class="text-sm leading-none font-semibold text-foreground mb-2">Intructions</h2>
+
+      <a
+        class="cursor-pointer underline text-sm text-blue-500 leading-none font-medium"
+        href="https://ghostbar.ai/docs/getting-started/setup"
+        target="_blank"
+      >
+        How to set up your Ghostbar
+        <ExternalLink name="lucide:external-link" class="w-3 h-3 inline-block" />
+      </a>
     </template>
   </Container>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.json";
-import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'url'
+
+const filesPathToExclude = ['src/docs'].map(src => {
+  return fileURLToPath(new URL(src, import.meta.url))
+})
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   plugins: [vue(), tailwindcss(), crx({ manifest })],
-});
+  build: {
+    rollupOptions: {
+      external: [...filesPathToExclude]
+    }
+  }
+})
